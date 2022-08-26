8c9ac21181b66c9289cdd816f423876ef230e515
Your Name <e@w.com>
2022-08-26 19:20:42 +0300

    Ввывод массива mxn вещественных чисел

---
+++ Task_47/Program.cs
@@ -0,0 +1,39 @@
+﻿// Создать двумерный массив размером m*n, заполенный случайными вещественными числами
+Console.WriteLine("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+
+double [,] arrayrandom (int m, int n)
+{
+
+   double[,] matrix = new double [m,n];
+   for (int i = 0; i < matrix.GetLength(0); i++)
+   {
+      for (int j = 0; j < matrix.GetLength(1); j++)
+      {
+         matrix[i,j] = new Random().Next(-100, 100)*0.1; 
+      }
+   }
+   return matrix;
+}
+double[,] result = arrayrandom(rows, columns);
+
+void PrintArray (double [,] inputMatrix)
+{
+
+     for (int i = 0; i < inputMatrix.GetLength(0); i++)
+   {
+      for (int j = 0; j < inputMatrix.GetLength(1); j++)
+      {
+         Console.Write(inputMatrix[i,j] + "\t");
+      }
+      Console.WriteLine();
+   }
+   
+}
+
+    PrintArray(result);
+    
+    
